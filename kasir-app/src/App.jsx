d46cec2d15020cba8748850541b84e5f48a1a6f9
cakcakgelo10 <rezaardiansyah343@gmail.com>
2025-06-16 01:11:54 +0700

    Initial project with Vite + React + Tailwind

---
+++ kasir-app/src/App.jsx
@@ -0,0 +1,14 @@
+const App = () => {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        TailwindCSS Berhasil!
+      </h1>
+      <p className="mt-4 text-lg text-gray-700">
+        Aplikasi kasir kamu sudah menggunakan TailwindCSS 🎉
+      </p>
+    </div>
+  )
+}
+
+export default App;
