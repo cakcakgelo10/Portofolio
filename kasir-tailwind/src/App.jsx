8fcf2190fce81dc387e08a48c5ee4c5df7da8c77
cakcakgelo10 <rezaardiansyah343@gmail.com>
2025-06-19 22:11:14 +0700

    minor edit App.jsx

--- kasir-tailwind/src/App.jsx
+++ kasir-tailwind/src/App.jsx
@@ -1,7 +1,20 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Login from "./pages/login";
 import Transaksi from "./pages/Transaksi";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
-  return <Transaksi />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/transaksi" element={<Transaksi />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
