8b334207cd9e35fcde682115852fa3709897392c
cakcakgelo10 <rezaardiansyah343@gmail.com>
2025-06-20 22:08:20 +0700

    update new code

--- kasir-tailwind/src/App.jsx
+++ kasir-tailwind/src/App.jsx
@@ -1,15 +1,32 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./pages/login";
+import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import Transaksi from "./pages/Transaksi";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/transaksi" element={<Transaksi />} />
+
+        {/* Lindungi halaman berikut */}
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/transaksi"
+          element={
+            <ProtectedRoute>
+              <Transaksi />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </Router>
   );
